bc81f1dff6e7a2febd4989e4747a9eb581d54d66
Carsten Munk <carsten.munk@jollamobile.com>
2013-05-31 11:31:08 +0200

    Add armv7thl port
    Signed-off-by: Carsten Munk <carsten.munk@jollamobile.com>

--- precheckin.sh
+++ precheckin.sh
@@ -2,7 +2,7 @@
 
 NAME=binutils
 SPECNAME=${NAME}.spec
-ARCHES="armv5tel armv6l armv7l armv7hl armv7nhl armv7tnhl mipsel i486 x86_64"
+ARCHES="armv5tel armv6l armv7l armv7hl armv7nhl armv7thl armv7tnhl mipsel i486 x86_64"
 
 for i in ${ARCHES} ; do
 # cross spec files
